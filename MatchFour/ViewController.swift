ef21ef694d20c6b475bc97eebc176b759af6952a
omersagy <omersagyemails@gmail.com>
2018-11-15 23:58:38 -0500

    spritekit view inside main view.

--- MatchFour/ViewController.swift
+++ MatchFour/ViewController.swift
@@ -6,15 +6,37 @@
 //  Copyright © 2018 Omer Sagy. All rights reserved.
 //
 
+import SpriteKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameView: SKView!
+    @IBOutlet weak var labelInput: UITextField!
+
+    let scene = SKScene(
+        size: CGSize(width: 277, height: 262))
+
+    let label = SKLabelNode(text: "SpriteKit")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.position = CGPoint(
+            x: scene.size.width / 2,
+            y: scene.size.height / 2)
+        scene.addChild(label)
+
+        render()
     }
 
+    @IBAction func labelChanged(_ sender: UITextField) {
+        label.text = sender.text
+        view.endEditing(true)
+        render()
+    }
 
+    func render() {
+        gameView.presentScene(scene)
+    }
 }
 
